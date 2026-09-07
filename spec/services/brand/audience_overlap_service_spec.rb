949079a77192fef85e2364b6ef2f6cbd33a05e14
himratesdev <himrate.sales@gmail.com>
2026-09-07 22:31:51 +0300

    test: brace seed hashes (Ruby 3 keyword-arg split)

--- spec/services/brand/audience_overlap_service_spec.rb
+++ spec/services/brand/audience_overlap_service_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe Brand::AudienceOverlapService, type: :service do
   let!(:c) { create(:channel, login: login_c) }
 
   before do
-    seed(login_a => [ alice, ns("bob"), ns("eve") ],
+    seed({login_a => [ alice, ns("bob"), ns("eve") ],
          login_b => [ alice, carol ],
-         login_c => [ carol, ns("dave") ])
+         login_c => [ carol, ns("dave") ] })
   end
 
   it "rejects fewer than 2 channels" do
